a3346c620f3d36a0455e14151daa29bfe9d5b545
shah khilan <shahkhilan2901@gmail.com>
2026-06-02 21:21:51 +0530

    app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+
+// Model Registrations
 require('../models/User');
 require('../models/Company');
 require('../models/Job');
 require('../models/Application');
 
+// Route Implementations
+const authRoutes = require('./authRoutes');
+const companyRoutes = require('./companyRoutes');
+const jobRoutes = require('./jobRoutes');
+
+// Mount Sub-routers
+router.use('/auth', authRoutes);
+router.use('/companies', companyRoutes);
+router.use('/jobs', jobRoutes);
+
+// Health check endpoint
 router.get('/health', (req, res) => {
-  res.json({ success: true, message: 'API is running' });
+  res.json({ success: true, message: 'API is running cleanly' });
 });
 
 module.exports = router;
